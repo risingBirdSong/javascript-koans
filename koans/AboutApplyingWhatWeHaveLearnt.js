a89b92220963905699be80f8cbafa49c84664429
risingbirdsong <heregoingthere@outlook.com>
2020-02-13 16:29:21 -0800

    attempt at extra credit, ran outta time, pseudo code

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -1,4 +1,55 @@
+import { CodeGenerator } from "@babel/generator";
+
 var _; //globals
+
+const findLargetsPrime = (numberArr, composite) => {};
+
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+const isPrime = num => {
+  if (num == 1) {
+    return false;
+  }
+  if (num === 2) {
+    return true;
+  }
+  let ranged = Array(num)
+    .fill(1)
+    .map((val, idx) => idx + 1);
+
+  let trimmed = ranged.slice(1, ranged.length - 1);
+  console.log("trimmed", trimmed);
+  //dont want number 1 or num itself
+  let isPrime = true;
+  trimmed.forEach(val => {
+    if (num % val === 0) {
+      isPrime = false;
+    }
+  });
+  return isPrime;
+};
+
+//("should find the largest prime factor of a composite number"
+
+const largestPrimeFactorOfComp = num => {
+  let possibilites = [2, 3];
+  let factors = [];
+  while (isPrime(num) === false) {
+    // pseudo CodeGenerator, ran outta time,
+    // if its even keep divide like this
+    // num = num / possibilites[0];
+    // if its not even shift the possibilites.
+    // continue with
+    // num = num / possibilites[0];
+    // factors.push(possibilites[0]);
+  }
+
+  return {
+    factors: factors,
+    num
+  };
+};
+
+console.log("factors", largestPrimeFactorOfComp(18));
 
 describe("About Applying What We Have Learnt", function() {
   var products;
@@ -68,8 +119,10 @@ describe("About Applying What We Have Learnt", function() {
     console.log(noNuts);
 
     productsICanEat = noNuts.filter(pizza => {
-      let mushrooms = pizza.ingredients.filter(ing => ing == "mushrooms");
-      if (mushrooms.length > 0) {
+      let mushrooms = pizza.ingredients.some(ing => ing == "mushrooms");
+      console.log("------------------------");
+      console.log(mushrooms);
+      if (mushrooms) {
         return false;
       } else return true;
     });
@@ -147,35 +200,14 @@ describe("About Applying What We Have Learnt", function() {
 
   /*********************************************************************************/
   /* UNCOMMENT FOR EXTRA CREDIT */
-  /*
-  it("should find the largest prime factor of a composite number", function () {
-
-  });
-
-  it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
-
-  });
-
-  it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
-
-
-  });
-
-  it("should find the difference between the sum of the squares and the square of the sums", function () {
-
-  });
-
-  it("should find the 10001st prime", function () {
-
-  });
-  */
 });
 
-const simpleArr = [1, 0, 1, 1, 0, 0, 1];
-const someTest = (arr, tester) => {
-  return arr.some(val => {
-    return val === tester;
-  });
-};
+// it("should find the largest prime factor of a composite number", function() {
+//   it("should find the largest palindrome made from the product of two 3 digit numbers", function() {});
 
-console.log("some test", someTest(simpleArr, 1));
+//   it("should find the smallest number divisible by each of the numbers 1 to 20", function() {});
+
+//   it("should find the difference between the sum of the squares and the square of the sums", function() {});
+
+//   it("should find the 10001st prime", function() {});
+// });
